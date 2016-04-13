5ef9e48d90da5e590511191d13e5205e64584bb7
William L. Marr III <hobbyist@wlmiii.net>
2016-04-13 14:06:54 -0400

    Create custom class for the table view cell

---
+++ fav_films/FilmCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilmCell.swift
+//  fav_films
+//
+//  Created by William L. Marr III on 4/13/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import UIKit
+
+class FilmCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
