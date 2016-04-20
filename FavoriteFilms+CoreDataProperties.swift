d1a128e6112479d9f3d9feb3b998ffaa317266b0
William L. Marr III <hobbyist@wlmiii.net>
2016-04-20 10:28:21 -0400

    Add film plot to data model

--- FavoriteFilms+CoreDataProperties.swift
+++ FavoriteFilms+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  FavoriteFilms+CoreDataProperties.swift
 //  fav_films
 //
-//  Created by William L. Marr III on 4/13/16.
+//  Created by William L. Marr III on 4/20/16.
 //  Copyright © 2016 William L. Marr III. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -14,9 +14,10 @@ import CoreData
 
 extension FavoriteFilms {
 
-    @NSManaged var filmTitle: String?
     @NSManaged var filmDescription: String?
-    @NSManaged var filmLink: String?
     @NSManaged var filmImage: NSData?
+    @NSManaged var filmLink: String?
+    @NSManaged var filmTitle: String?
+    @NSManaged var filmPlot: String?
 
 }
